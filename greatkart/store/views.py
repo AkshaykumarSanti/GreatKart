a0627f4919836e4f1771fd5746abd374ed25bee0
Akshaykumar Santi <santiakshay248@gmail.com>
2026-07-22 17:04:47 +0530

    Day 08: Implement product search and pagination

--- greatkart/store/views.py
+++ greatkart/store/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 from category.models import Category
 from carts.views import _cart_Id, CartItem
-
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
+from django.http import HttpResponse
+from django.db.models import Q
 
 
 def store(request, category_slug = None):
@@ -12,13 +14,19 @@ def store(request, category_slug = None):
     if category_slug != None:
         categories = get_object_or_404(Category, slug = category_slug)
         products = Product.objects.filter(category = categories, is_available = True)
+        paginator = Paginator(products, 1)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
     else:
-        products = Product.objects.all().filter(is_available = True)
+        products = Product.objects.all().filter(is_available = True).order_by('id')
+        paginator = Paginator(products, 3)
+        page = request.GET.get('page')
+        paged_products = paginator.get_page(page)
         product_count = products.count()
 
     context = {
-        'products': products,
+        'products': paged_products,
         'product_count' : product_count,
     }
     return render(request, 'store/store.html', context)
@@ -39,3 +47,25 @@ def product_detail(request, category_slug, product_slug):
     }
 
     return render(request, 'store/product_detail.html', context)
+
+def search(request):
+    products = Product.objects.none()
+    product_count = 0
+
+    keyword = request.GET.get('keyword')
+    if keyword:
+        products = Product.objects.order_by('-created_date').filter(
+            Q(description__icontains=keyword) | Q(product_name__icontains=keyword)
+        )
+
+    product_count = products.count()
+
+    paginator = Paginator(products, 3)
+    page = request.GET.get('page')
+    paged_products = paginator.get_page(page)
+
+    context = {
+        'products': paged_products,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
